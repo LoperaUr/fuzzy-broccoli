461d24b23db59d23fec5be6129eaa3ebd335b84d
LoperaUr <manuellopera612@gmail.com>
2023-09-29 19:10:35 -0500

    first commit

---
+++ src/main/java/com/fuzzybroccoli/FuzzyBroccoliApplication.java
@@ -0,0 +1,13 @@
+package com.fuzzybroccoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzyBroccoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzzyBroccoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuzzybroccoli/FuzzyBroccoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuzzybroccoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzzyBroccoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
