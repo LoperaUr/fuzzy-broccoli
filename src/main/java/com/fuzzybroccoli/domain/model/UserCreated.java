d157464491e8f3a162e4397da39f81f9af8545af
LoperaUr <manuellopera612@gmail.com>
2023-10-06 21:07:39 -0500

    template create for create traces

---
+++ src/main/java/com/fuzzybroccoli/domain/model/UserCreated.java
@@ -0,0 +1,35 @@
+package com.fuzzybroccoli.domain.model;
+
+import java.time.LocalDateTime;
+
+public class UserCreated {
+
+    private String email;
+    private LocalDateTime createdAt;
+    private String createdBy;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+}
